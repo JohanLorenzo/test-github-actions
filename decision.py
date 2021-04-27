b966dccd1d02350f16342766fce8de670e9043af
Johan Lorenzo <johan.lorenzo@malt.com>
2021-04-27 09:39:29 +0200

    Downgrade tag warning to info

--- decision.py
+++ decision.py
@@ -108,7 +108,7 @@ def _get_docker_image_hash_on_registry(docker_repository, docker_image_tag):
         raise
 
     image_hash_on_registry = index_request.headers["Docker-Content-Digest"]
-    log.warning(f'Tag "{docker_image_tag}" already exists. Registry has this hash associated to it: {image_hash_on_registry}')
+    log.info(f'Tag "{docker_image_tag}" already exists. Registry has this hash associated to it: {image_hash_on_registry}')
     return image_hash_on_registry
 
 
